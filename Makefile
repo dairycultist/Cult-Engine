b705149126f41de0435f02c950a61e3e8f12f7ba
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-01-29 20:17:51 -0500

    Intermediary state for client rendering implementation

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
 .PHONY: all clean
 
+UNAME := $(shell uname)
+
+ifeq ($(UNAME), Darwin) # macOS
+	GCCFLAGS = -lGLEW -framework OpenGL
+else ifeq ($(UNAME), Linux)
+	GCCFLAGS = -lGLEW -lGL -lm
+else
+	$(error Unsupported OS: $(UNAME))
+endif
+
+# recipes
 all: client.out server.out
 
 client: client.out
@@ -9,7 +20,7 @@ server: server.out
 	@./server.out 127.0.0.1 25565
 
 client.out: client.c packet.c
-	@gcc client.c -o client.out
+	@gcc client.c -o client.out $(GCCFLAGS) $(shell pkg-config --cflags --libs sdl2 SDL2_image)
 
 server.out: server.c packet.c
 	@gcc server.c -o server.out
--- client.c
+++ client.c
@@ -7,10 +7,21 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
 #include "packet.c"
+
+typedef struct {
+
+	// contains socket, information about the gamestate (like entities and objs), and whether or not the session has ended
+
+} SessionInfo;
 
 int main(int argc, char **argv) {
 
+	// create server connection
 	if (argc != 3) {
 		printf("Format: %s <IP> <PORT>\n", argv[0]);
 		return 0;
@@ -40,6 +51,8 @@ int main(int argc, char **argv) {
 		}
 	}
 
+	// create two threads; one for handling server messages and one for server-independent logic (including rendering!)
+
 	// test transmission
 	static Packet packet;
 
@@ -57,4 +70,88 @@ int main(int argc, char **argv) {
     close(fd);
 
     return 0;
+}
+
+
+
+int server_independent() {
+
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		fprintf(stderr, "Could not initialize SDL (%s)\n", SDL_GetError());
+		return 1;
+	}
+
+	// init OpenGL
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+	// create the window
+	SDL_Window *window = SDL_CreateWindow("Client", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 400, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+	if (!window) {
+		fprintf(stderr, "Could not create window (%s)\n", SDL_GetError());
+		return 1;
+    }
+
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+
+	if (!context) {
+		fprintf(stderr, "Could not create context (%s)\n", SDL_GetError());
+		return 1;
+    }
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	// enable depth buffer
+	glEnable(GL_DEPTH_TEST);
+
+	// enable backface culling
+	glEnable(GL_CULL_FACE);
+	glFrontFace(GL_CCW);
+
+	// initialize rendering
+	// initialize_shaders();
+	// initialize_perspective(2.0);
+
+	// process events until window is closed
+	SDL_Event event;
+	int running = 1;
+
+	while (running) {
+
+		while (SDL_PollEvent(&event)) {
+
+			if (event.type == SDL_QUIT) {
+
+				running = 0;
+
+				// TODO send disconnect packet
+
+			} else if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_RESIZED) {
+
+				glViewport(0, 0, event.window.data1, event.window.data2);
+				// initialize_perspective(event.window.data1 / (float) event.window.data2);
+			
+			} else {
+
+				// input handling
+			}
+		}
+
+		// client-side (server-independent) logic
+
+		SDL_GL_SwapWindow(window);
+		SDL_Delay(1000 / 60);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
+
+	// free everything
+	SDL_DestroyWindow(window);
+	SDL_GL_DeleteContext(context);
+	SDL_Quit();
+
+	return 0;
 }
