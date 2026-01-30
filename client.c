a53d210c65fe587d63aac71ecb6c1e3986b80768
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-01-29 20:32:27 -0500

    Basic implementation of client-side graphics

--- client.c
+++ client.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <pthread.h>
+#include <stdint.h>
 
-#include <sys/socket.h>
 #include <arpa/inet.h>
 
 #include <GL/glew.h>
@@ -15,70 +17,44 @@
 
 typedef struct {
 
-	// contains socket, information about the gamestate (like entities and objs), and whether or not the session has ended
+	int fd; // socket
+	int BOOL_active; // whether or not the session has ended
+
+	// will also contain information about the gamestate (like entities and objs)
 
 } SessionInfo;
 
-int main(int argc, char **argv) {
+void *handle_server(void *session_info_void) {
 
-	// create server connection
-	if (argc != 3) {
-		printf("Format: %s <IP> <PORT>\n", argv[0]);
-		return 0;
-	}
-	
-    int fd = socket(AF_INET, SOCK_STREAM, 0);
+	SessionInfo *session_info = (SessionInfo *) session_info_void;
 
-    if (fd < 0) {
-        perror("socket creation failed");
-        return errno;
-    }
-
-	{
-		struct sockaddr_in serv_addr;
-
-		serv_addr.sin_family = AF_INET;
-		serv_addr.sin_port = htons(atoi(argv[2]));
-
-		if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0) {
-			perror("Invalid address.");
-			return errno;
-		}
-
-		if (connect(fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-			perror("Connection failed.");
-			return errno;
-		}
+	while (session_info->BOOL_active) {
+		usleep(1000000);
 	}
 
-	// create two threads; one for handling server messages and one for server-independent logic (including rendering!)
+	// // test transmission
+	// static Packet packet;
 
-	// test transmission
-	static Packet packet;
+	// packet.packet_type = P_PING;
+	// packet.big_body_size = strlen("Hello there, server!") + 1;
+	// packet.big_body = "Hello there, server!";
 
-	packet.packet_type = P_PING;
-	packet.big_body_size = strlen("Hello there, server!") + 1;
-	packet.big_body = "Hello there, server!";
+	// write_packet(fd, &packet);
 
-	write_packet(fd, &packet);
+	// read_packet(fd, &packet);
 
-	read_packet(fd, &packet);
+	// printf("Heard pong: %s\n", packet.big_body);
 
-	printf("Heard pong: %s\n", packet.big_body);
-
-    // close the socket
-    close(fd);
-
-    return 0;
+	return NULL;
 }
 
+void *server_independent(void *session_info_void) {
 
-
-int server_independent() {
+	SessionInfo *session_info = (SessionInfo *) session_info_void;
 
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		fprintf(stderr, "Could not initialize SDL (%s)\n", SDL_GetError());
-		return 1;
+		exit(1);
 	}
 
 	// init OpenGL
@@ -92,14 +68,14 @@ int server_independent() {
 
 	if (!window) {
 		fprintf(stderr, "Could not create window (%s)\n", SDL_GetError());
-		return 1;
+		exit(2);
     }
 
 	SDL_GLContext context = SDL_GL_CreateContext(window);
 
 	if (!context) {
 		fprintf(stderr, "Could not create context (%s)\n", SDL_GetError());
-		return 1;
+		exit(3);
     }
 
 	glewExperimental = GL_TRUE;
@@ -118,15 +94,15 @@ int server_independent() {
 
 	// process events until window is closed
 	SDL_Event event;
-	int running = 1;
 
-	while (running) {
+	while (session_info->BOOL_active) {
 
+		// events (key presses, mouse movements, etc)
 		while (SDL_PollEvent(&event)) {
 
 			if (event.type == SDL_QUIT) {
 
-				running = 0;
+				session_info->BOOL_active = 0;
 
 				// TODO send disconnect packet
 
@@ -141,8 +117,9 @@ int server_independent() {
 			}
 		}
 
-		// client-side (server-independent) logic
+		// TODO logic
 
+		// rendering stuff
 		SDL_GL_SwapWindow(window);
 		SDL_Delay(1000 / 60);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -153,5 +130,59 @@ int server_independent() {
 	SDL_GL_DeleteContext(context);
 	SDL_Quit();
 
-	return 0;
+	return NULL;
+}
+
+int main(int argc, char **argv) {
+
+	if (argc != 3) {
+		printf("Format: %s <IP> <PORT>\n", argv[0]);
+		return 0;
+	}
+
+	static SessionInfo session_info;
+	session_info.BOOL_active = 1;
+	
+	// create server connection
+    session_info.fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (session_info.fd < 0) {
+        perror("socket creation failed");
+        return errno;
+    }
+
+	{
+		struct sockaddr_in serv_addr;
+
+		serv_addr.sin_family = AF_INET;
+		serv_addr.sin_port = htons(atoi(argv[2]));
+
+		if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0) {
+			perror("Invalid address.");
+			return errno;
+		}
+
+		if (connect(session_info.fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+			perror("Connection failed.");
+			return errno;
+		}
+	}
+
+	// create a separate thread for handling server packets
+	// (the main thread will handle server-independent logic -- including rendering!)
+	pthread_t server_handler;
+
+	if (pthread_create(&server_handler, NULL, handle_server, &session_info)) {
+
+		perror("Failed to create a thread.\n");
+		return 1;
+	}
+
+	server_independent(&session_info);
+	pthread_join(server_handler, NULL);
+
+    // close the socket
+    close(session_info.fd);
+
+    return 0;
 }
